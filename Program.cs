9c7ae0fe920d418db5143f3f5fc383f2ca45add6
Leonid <rockwil91@gmail.com>
2023-09-30 23:58:57 +0300

    дбавлен метод принимающий значения элементов массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов в массиве");
+
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+int maxChar = 3;
+int maxSize = 0;
+
+ 
+string[] FillArray (string[] arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите значение элемента массива");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+FillArray(array);
