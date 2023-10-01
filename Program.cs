46dff0aa70d01f1f9cb00e2d73e0b7a72b5c1928
Leonid <rockwil91@gmail.com>
2023-10-01 23:12:41 +0300

    добавлен метод создания нового массива + правка *удален предыдущий метод

--- Program.cs
+++ Program.cs
@@ -1,19 +1,52 @@
 ﻿Console.WriteLine("Введите количество элементов в массиве");
 
 int size = Convert.ToInt32(Console.ReadLine());
+
 string[] array = new string[size];
 int maxChar = 3;
 int maxSize = 0;
 
- 
-string[] FillArray (string[] arr)
+
+
+for (int i = 0; i < size; i++)
 {
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine("Введите значение элемента массива");
-        arr[i] = Console.ReadLine();
-    }
-    return arr;
+    Console.WriteLine("Введите значение элемента массива");
+    array[i] = Console.ReadLine();
 }
 
-FillArray(array);
+
+for (int m = 0; m < size; m++)
+{
+    if (array[m].Length <= maxChar)
+        maxSize++;
+}
+
+string[] FixedArray(string[] arr2, int charCount, int newArraySize)
+{
+    string[] tempArray = new string[newArraySize];
+    int n = 0;
+    for (int l = 0; l < newArraySize; l++)
+    {
+        if (arr2[l].Length <= charCount)
+        {
+            tempArray[n] = arr2[l];
+            n++;
+        }
+    }
+    return tempArray;
+}
+
+string[] newArray = FixedArray(array, maxChar, maxSize);
+Console.Clear();
+
+Console.Write("[");
+for (int p = 0; p < size - 1; p++)
+{
+    Console.Write($"'{array[p]}', ");
+}
+Console.Write($"'{array[size - 1]}' ] -> [");
+for (int i = 0; i < maxSize - 1; i++)
+{
+    Console.Write($"'{newArray[i]}', ");
+}
+Console.Write($"'{newArray[maxSize - 1]}' ]");
